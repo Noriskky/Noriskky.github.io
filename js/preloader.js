74c7b0039c84516365d79baa0dc6fbab218e86db
Noriskky <Lefoxx86@gmail.com>
2023-02-17 00:17:21 +0100

    daws

--- js/preloader.js
+++ js/preloader.js
@@ -1,10 +1,22 @@
-$(document).ready(function() {
-    setTimeout(function() {
-        $('body').addClass('loaded');
-        // Once the container has finished, the scroll appears
-        if ($('body').hasClass('loaded')) {
-            // It is so that once the container is gone, the entire preloader section is deleted
-            $('.wrapper').delay(9000).queue(function() {
-                $(this).remove();
-            });}
-    }, 3000);});
+$(document).ready(function () {
+init()
+});
+
+async function init() {
+    document.getElementById("header").style.display = 'none';
+    document.body.style.display = 'none';
+    document.body.style.overflowY = 'hidden';
+    await delay(1000)
+    await delay(1000)
+    $("body").fadeIn(300);
+    $("#wrapper").fadeOut(10);
+    window.scroll({top: 99999});
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+    document.getElementById("header").style.display = '';
+    document.body.style.overflowY = '';
+    document.getElementById("wrapper").style.display = 'none';
+}
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
