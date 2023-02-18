826b05ebeacd85d71ffd3d05f6e36e939956de3e
Noriskky <Lefoxx86@gmail.com>
2023-02-18 13:56:44 +0100

    dsad
    dsa

--- js/header.js
+++ js/header.js
@@ -1,3 +1,12 @@
 const header = document.getElementById("header");
 
-header.blur()
+window.onscroll = function() { sticky() };
+
+var sticky = navbar.offsetTop;
+function sticky() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,3 @@
+function scroltotop() {
+    window.scroll({top: 0});
+}
