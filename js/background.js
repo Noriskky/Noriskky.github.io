23805f0832ca64fb1372fa88a9e1cb2ef4ba2bb4
Noriskky <Lefoxx86@gmail.com>
2023-03-12 17:42:56 +0100

    sd

--- js/background.js
+++
@@ -1,9 +0,0 @@
-const blob = document.getElementById("blob");
-
-document.body.onpointermove = event => {
-    const { clientX, clientY } = event;
-        blob.animate({
-            left: `${clientX}px`,
-            top: `${clientY}px`
-        }, {duration: 200, fill: "forwards"});
-}
